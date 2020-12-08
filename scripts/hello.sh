3b47b6dd5d52de6af8b5b4364a6a41a5e2785857
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 13:16:59 -0300

    13:16:59 - Shellscript

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -5,4 +5,7 @@
 # Para habilitarmos a execução do arquivo, precisamos rodar o comando :   chmod +x <NOME_DO_ARQUIVO>
 # Utilizando shell script podemos referenciar os interpretadores de várias linguagens e escrever o código com ela.
 
+# O ideal é que cada script retorne um exit code, para que o interpretador saiba que o código finalizou sua execução,
+# ou se gerou algum erro.
+
 echo "Hello world from bash!";
