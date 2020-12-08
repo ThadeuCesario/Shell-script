33ed249084299f2d39a7033dfb4b82c76574a4c3
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 12:32:32 -0300

    12:32:32 - Shellscript

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Na primeira linha estamos declarando qual a forma de processamento deste arquivo.
-#
+# Na primeira linha estamos declarando qual a forma de processamento deste arquivo. Neste caso, é o bash.
+# Sem o '!', seria apenas uma linha de comentário. Portanto precisamos inserir o '!' na declaração do processador. 
