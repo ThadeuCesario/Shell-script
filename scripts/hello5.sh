d96aa5bcc33ad03d978af9192580e82dbbea4120
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 12:50:38 -0300

    12:50:38 - Shellscript

--- scripts/hello5.sh
+++ scripts/hello5.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-# Nesse arquivo estamos utilizando o NODE para rodar este arquivo. 
+#Nesse arquivo estamos utilizando o NODE para rodar este arquivo. 
 
 console.log("Hello world from NODE");
