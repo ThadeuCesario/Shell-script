26c09b73f829ef32f31c1bcf6f67d503c8055285
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 13:26:17 -0300

    13:26:17 - Shellscript

--- scripts/params.sh
+++ scripts/params.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Para acessarmos uma variável utilizamos o $.
+#
 # Para acessarmos a quantidade de parametros passadas, utilizamos o #.
+#
+# Quando utilizamos o $0, significa que estamos acessando o primeiro parametro. Sendo que o primeiro parametro, 
+# será sempre o arquivo que estamos executando.
 
 
 echo "Primeiro parametro: $0";
