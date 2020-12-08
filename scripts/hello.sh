c72aa50c1dc33c4f81f6d33f7a59d60db22523bc
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 12:33:57 -0300

    12:33:57 - Shellscript

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -3,4 +3,4 @@
 # Na primeira linha estamos declarando qual a forma de processamento deste arquivo. Neste caso, é o bash.
 # Sem o '!', seria apenas uma linha de comentário. Portanto precisamos inserir o '!' na declaração do processador. 
 
-echo "Hello world from bash.";
+echo "Hello world from bash!";
