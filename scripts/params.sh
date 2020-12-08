60b0d832849ebc77cec799516d53b1f546dcab4f
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 13:29:48 -0300

    13:29:48 - Shellscript

--- scripts/params.sh
+++ scripts/params.sh
@@ -5,7 +5,7 @@
 # Para acessarmos a quantidade de parametros passadas, utilizamos o #.
 #
 # Quando utilizamos o $0, significa que estamos acessando o primeiro parametro. Sendo que o primeiro parametro, 
-# será sempre o arquivo que estamos executando.
+# será sempre o arquivo que estamos executando
 
 echo "Primeiro parametro: $0";
 echo "Quantidade de parametros: $#";
