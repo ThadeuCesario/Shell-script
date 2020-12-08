882106de77515dbf5cfcfc4b6f0146588c05d4d4
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 13:24:32 -0300

    13:24:32 - Shellscript

--- scripts/params.sh
+++ scripts/params.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# Para acessarmos uma variável utilizamos o $.
+# Para acessarmos a quantidade de parametros passadas, utilizamos o #.
+
+
+echo "Primeiro parametro: $0";
+echo "Quantidade de parametros: $#"
