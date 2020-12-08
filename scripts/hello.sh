50407339585c814c92268db174719335914ce098
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 12:34:47 -0300

    12:34:47 - Shellscript

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -2,5 +2,6 @@
 
 # Na primeira linha estamos declarando qual a forma de processamento deste arquivo. Neste caso, é o bash.
 # Sem o '!', seria apenas uma linha de comentário. Portanto precisamos inserir o '!' na declaração do processador. 
+# Para habilitarmos a execução do arquivo, precisamos rodar o comando :   chmod +x <NOME_DO_ARQUIVO>
 
 echo "Hello world from bash!";
